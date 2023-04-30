d3682ef5e983e77569440d8a12b4d0134d523caf
unknown <ajemark0110@gmail.com>
2023-04-30 19:09:41 +0100

    UD login integrated

--- js/main.js
+++ js/main.js
@@ -1,13 +1,65 @@
+
+
 const footer__question = document.querySelectorAll(".footer__question");
 
-for (let item of footer__question){
-    item.addEventListener("click", function(e){
+for (let item of footer__question) {
+    item.addEventListener("click", function (e) {
         let el = e.target;
         console.log(el)
         let maiParentEl = el.closest("div.footer__inner__item");
         maiParentEl.querySelector(".footer__inner__body").classList.toggle("show");
-        
+
     });
+}
+
+
+
+
+const handleLogin = () => {
+    const UAuth = uDauth({
+        clientID: "37a2f8494930007072047040174013hk20",
+        redirectUri: `${location.origin}`,
+        scope: "openid wallet email profile:optional social:optional"
+    })
+
+    let uDuser;
+
+    try {
+        UAuth.user()
+            .then((user) => {
+                uDuser = user
+                console.log(user)
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+    } catch (err) {
+        console.log(err)
+    }
+
+    console.log(uDuser)
+
+    if (uDuser) logoutUD(UAuth)
+    else loginUD(UAuth)
+
+}
+
+const loginUD = async (UAuth) => {
+    try {
+        await UAuth.loginWithPopup()
+        location.reload()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const logoutUD = async (UAuth) => {
+    try {
+        await UAuth.logout()
+        location.reload()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
